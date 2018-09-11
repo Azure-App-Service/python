6630669939de3de3b46e5871ec201536fd54aa6e
root <root@tuxedolinuxbuildvm.i1t5xd3p4kue5apare5pkznajf.cx.internal.cloudapp.net>
2018-09-11 22:07:02 +0000

    Updates to 3.7 to do auto-detect flask app.py

--- 3.7.0/build.sh
+++ 3.7.0/build.sh
@@ -6,9 +6,3 @@ buildnumber=${4-$(date -u +"%y%m%d%H%M")}
 docker build --no-cache -t "$1"/python:3.7.0_"$buildnumber" .
 docker tag "$1"/python:3.7.0_"$buildnumber" "$1"/python:latest
 
-#docker login -u "$2" -p "$3"
-
-#docker push "$1"/python:3.7.0_"$buildnumber"
-#docker push "$1"/python:latest
-
-#docker logout
