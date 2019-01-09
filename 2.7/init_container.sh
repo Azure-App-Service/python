534725badcc55c3128afb3f61bc2e3e8ecff0256
JennyLawrance <jennylaw@microsoft.com>
2019-01-08 16:27:51 -0800

    Update to init script

--- 2.7/init_container.sh
+++ 2.7/init_container.sh
@@ -18,24 +18,34 @@ cat /etc/motd
 
 service ssh start
 
-
 # Get environment variables to show up in SSH session
 eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
 
-echo "$@" > /opt/startup/startupCommand
+oryxArgs='-appPath /home/site/wwwroot -output /opt/startup/startup.sh -virtualEnvName antenv2.7 -defaultApp /opt/defaultsite'
 
-STARTUPCOMMAND="$(cat /opt/startup/startupCommand)"
-
-FILE="$@"
-if [ -f $FILE ]; then
-    STARTUPCOMMAND=$(head -1 $FILE)
-elif
-    STARTUPCOMMAND=''
+if [ $# -eq 0 ]; then
+    echo 'App Command Line not configured, will attempt auto-detect'
+else
+    echo "Site's appCommandLine: $@" 
+    if [ $# -eq 1 ]; then
+        echo "Checking of $1 is a file"
+        if [ -f $1 ]; then
+            echo "$1 file exists on disk, reading its contents to run as startup arguments"
+            fileContents=$(head -1 $1)
+            echo "Contents of startupScript: $fileContents"
+            oryxArgs+=' -userStartupCommand $fileContents'
+        else
+            oryxArgs+=' -userStartupCommand $1'
+        fi
+    else
+        oryxArgs+=" -userStartupCommand $@"
+    fi
 fi
 
+echo "Launching oryx with: $oryxArgs"
 #invoke oryx to generate startup script
-oryx -appPath /home/site/wwwroot -userStartupCommand $STARTUPCOMMAND -output /opt/startup/startup.sh -virtualEnvName antenv2.7 -defaultApp /opt/defaultSite
-chmod 777 /opt/startup/startup.sh
+eval "oryx $oryxArgs"
+chmod +x /opt/startup/startup.sh
 
 #launch startup script
 /opt/startup/startup.sh
