e51db2904f437c517ed8d9af31e38cfcfae4ca8f
blimp <patle@microsoft.com>
2019-06-25 16:16:57 -0700

    blimp

--- 2.7/init_container.sh
+++ 2.7/init_container.sh
@@ -20,7 +20,7 @@ sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
 service ssh start
 
 # Get environment variables to show up in SSH session
-eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
 echo "$@" > /opt/startup/startupCommand
 chmod 755 /opt/startup/startupCommand
