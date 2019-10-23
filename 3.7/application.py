9b2aa6a659ef99a0b8b0081bdc5257a19dffccd4
blimp <patle@microsoft.com>
2019-10-22 17:59:25 -0700

    blimp

--- 3.7/application.py
+++ 3.7/application.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import os
 app = Flask(__name__, static_folder='/opt/defaultsite')
 
 @app.route('/')
 def root():
-    return app.send_static_file('hostingstart.html')
+    if os.path.isdir('/home/site/deployments') and len(next(os.walk('/home/site/deployments'))[1]) > 1:
+        return app.send_static_file('hostingstart_dep.html')
+    else:
+        return app.send_static_file('hostingstart.html')
